e85413468d96f0ae4df55234d303ad4825057386
Hydro4 <46918065+Hydro4@users.noreply.github.com>
2024-12-21 20:43:19 -0500

    binary tree for tennis game

---
+++ Hypothesis Testing/LinkedBinaryTree.py
@@ -0,0 +1,25 @@
+class LinkedBinaryTree:
+    class Node:
+        def __init__(self, data, left=None, right=None, left_weight=None, right_weight=None):
+            self.data = data
+            self.left = left
+            if (left is not None):
+                self.left.parent = self
+            self.right = right
+            if (right is not None):
+                self.right.parent = self
+            self.parent = None
+
+            self.left_weight = left_weight
+            self.right_weight = right_weight
+
+
+    def __init__(self, root=None):
+        self.root = root
+        self.size = self.count_nodes()
+
+    def __len__(self):
+        return self.size
+
+    def is_empty(self):
+        return (len(self) == 0)
