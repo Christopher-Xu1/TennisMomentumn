f57ed1c4305bd6a9784709a6936d95463e2eae28
Hydro4 <46918065+Hydro4@users.noreply.github.com>
2024-12-31 14:23:55 +0800

    fix game_tree to have consistent data names

--- Hypothesis Testing/game_tree.py
+++ Hypothesis Testing/game_tree.py
@@ -36,7 +36,7 @@ def construct_tree(tree):
     n40_15 = n40_0.left
 
     n30_15.right = n40_15
-    n30_15.left = LinkedBinaryTree().Node("30-30, DEUCE")
+    n30_15.left = LinkedBinaryTree().Node("30-30")
     nDeuce = n30_15.left
 
     n15_30.right = nDeuce
@@ -48,11 +48,11 @@ def construct_tree(tree):
     nLose = n0_40.left
 
     n40_15.right = nWin
-    n40_15.left = LinkedBinaryTree().Node("40-30, A1")
+    n40_15.left = LinkedBinaryTree().Node("40-30")
     nA_1 = n40_15.left
 
     nDeuce.right = nA_1
-    nDeuce.left = LinkedBinaryTree().Node("30-40, A2")
+    nDeuce.left = LinkedBinaryTree().Node("30-40")
     nA_2 = nDeuce.left
 
     n15_40.right = nA_2
